7a67091120541a9a318e3b0a1235bcafceea71c7
Leway Colin <colin4124@gmail.com>
2022-09-19 12:54:12 +0800

    Refactor Makefile use relative path

--- scratchip/assets/project.mk
+++ scratchip/assets/project.mk
@@ -1,3 +1,12 @@
-base_dir={prj_dir}
+
+base_dir=.
+base_dir_rel=../..
+scratchip_dir=$(base_dir)/.scratchip
+scratchip_dir_rel=$(base_dir_rel)/.scratchip
+
+export MILL_LIB=$(scratchip_dir_rel)/jars
+export COURSIER_CACHE=$(scratchip_dir_rel)/cache
+
 verilog:
-	make -C $(base_dir)/hw/chisel verilog
+	chmod +x $(scratchip_dir)/mill
+	cd $(base_dir)/hw/chisel; $(scratchip_dir_rel)/mill chisel.run -td $(base_dir_rel)/builds
